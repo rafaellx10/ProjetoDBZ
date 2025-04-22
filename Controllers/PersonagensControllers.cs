b63145276ea57b73f38fc397dab93e39b30fa351
Rafael Xavier <rafaellx10@gmail.com>
2025-04-21 23:03:00 -0300

    Refactor PersonagensControllers and Personagem model to enhance API functionality and validation

--- Controllers/PersonagensControllers.cs
+++ Controllers/PersonagensControllers.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjetoDBZ.Data;
 using ProjetoDBZ.Models;
 
@@ -19,12 +17,61 @@ namespace ProjetoDBZ.Controllers
     }
 
     [HttpPost]
-    public async Task<IActionResult> AddPersonagem(Personagem personagem)
+    public async Task<IActionResult> AddPersonagem([FromBody] Personagem personagem)
     {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
       _appDbContext.DBZ.Add(personagem);
       await _appDbContext.SaveChangesAsync();
 
+      return Created("Personagem criado com sucesso!", personagem);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagens()
+    {
+      var personagens = await _appDbContext.DBZ.ToListAsync();
+      return Ok(personagens);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPersonagem(int id)
+    {
+      var personagem = await _appDbContext.DBZ.FindAsync(id);
+      if (personagem == null)
+      {
+        return NotFound("Personagem não encontrado");
+      }
       return Ok(personagem);
     }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePersonagem(int id, [FromBody] Personagem updatedPersonagem)
+    {
+      var personagemExists = await _appDbContext.DBZ.FindAsync(id);
+      if (personagemExists == null)
+      {
+        return NotFound("Personagem não encontrado");
+      }
+      _appDbContext.Entry(personagemExists).CurrentValues.SetValues(updatedPersonagem);
+      await _appDbContext.SaveChangesAsync();
+      return StatusCode(201, personagemExists);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePersonagem(int id)
+    {
+      var personagem = await _appDbContext.DBZ.FindAsync(id);
+      if (personagem == null)
+      {
+        return NotFound("Personagem não encontrado");
+      }
+      _appDbContext.DBZ.Remove(personagem);
+      await _appDbContext.SaveChangesAsync();
+      return Ok("Personagem deletado com sucesso");
+    }
+
   }
 }
--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,19 @@ namespace ProjetoDBZ.Models
 {
   public class Personagem
   {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do personagem é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O nome do personagem deve ter no máximo 50 caracteres.")]
+    [MinLength(3, ErrorMessage = "O nome do personagem deve ter no mínimo 3 caracteres.")]
+    [DataType(DataType.Text)]
+    [DisplayFormat(ConvertEmptyStringToNull = true)]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "O tipo do personagem é obrigatória.")]
+    [StringLength(50, ErrorMessage = "O tipo do personagem deve ter no máximo 50 caracteres.")]
+    [MinLength(3, ErrorMessage = "O tipo do personagem deve ter no mínimo 3 caracteres.")]
     public string Type { get; set; }
   }
 }
